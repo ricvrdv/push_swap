576fae0d7ff02b41e587410a71ebfca344fabf0d
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c1r11s6.42porto.com>
2025-01-10 16:08:04 +0000

    Added repository

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 18:30:17 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/01/08 18:37:30 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_stack_node
+{
+	int					value;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+#endif
